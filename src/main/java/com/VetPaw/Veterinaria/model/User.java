7f37539ba5b1f8b82217fce66594cc416fb64e1a
Jero <jeronimobazanah@hotmail.com>
2025-08-16 19:37:25 -0300

    Last changes

--- src/main/java/com/VetPaw/Veterinaria/model/User.java
+++ src/main/java/com/VetPaw/Veterinaria/model/User.java
@@ -10,11 +10,60 @@ import lombok.Data;
 @Entity
 public class User {
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassw() {
+        return passw;
+    }
+
+    public void setPassw(String passw) {
+        this.passw = passw;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
     private String apellido;
     private String celular;
+    private String passw;
+    private String rol;
 
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
 }
