e921db2afd5dbf40f120df89348aa42f851dc26d
Jero <jeronimobazanah@hotmail.com>
2025-09-23 18:18:31 -0300

    Ultimos cambios de hoy

--- src/main/java/com/VetPaw/Veterinaria/model/Usuario.java
+++ src/main/java/com/VetPaw/Veterinaria/model/Usuario.java
@@ -12,6 +12,7 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String documento;
     private String nombre;
     private String apellido;
