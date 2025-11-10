64108c1e68ede548d6d177ab4f07e1085da926e2
Jero <jeronimobazanah@hotmail.com>
2025-11-10 19:58:48 -0300

    Funciones agregadas

---
+++ src/main/java/com/VetPaw/Veterinaria/model/Tratamiento.java
@@ -0,0 +1,50 @@
+package com.VetPaw.Veterinaria.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Transient;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+@Data
+@Entity
+public class Tratamiento {
+
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private LocalDateTime fechaInicio;
+    private LocalDateTime fechaFin;
+    private int frecuenciaHoras;
+    private LocalDateTime ultimaAplicacion;
+    private LocalDateTime proximaDosis;
+    private String estado;
+
+    @ManyToOne
+    @JoinColumn(name = "mascota_id")
+    private Mascota mascota;
+
+    @ManyToOne
+    @JoinColumn(name = "veterinario_id")
+    private Veterinario veterinario;
+
+
+
+    @Transient
+    public boolean estaAtrasado(){
+        LocalDateTime proxima = getProximaDosis();
+        return proxima != null && LocalDateTime.now().isAfter(proxima); //comapra dos momentos en el tiempo para determinar si es posterior al otro
+    }
+
+    @Transient
+    public long getHorasAtraso(){
+        if(!estaAtrasado()){
+            return 0;
+        }
+        return ChronoUnit.HOURS.between(getProximaDosis(), LocalDateTime.now());
+    }
+}
